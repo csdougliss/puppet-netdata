2fe1a6279fb993d6caf6199edd36132dbc5d6efe
Craig Carnell <craig_carnell@hotmail.com>
2017-01-31 11:16:26 +0000

    we have no service yet

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,9 +11,9 @@ class netdata(
 ) inherits netdata::params {
   contain netdata::install
   contain netdata::config
-  contain netdata::service
+  #contain netdata::service
 
   Class['::netdata::install'] ->
-  Class['::netdata::config'] ~>
-  Class['::netdata::service']
+  Class['::netdata::config'] # ~>
+  #Class['::netdata::service']
 }
